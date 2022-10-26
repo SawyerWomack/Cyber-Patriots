a806ef16cc306698fad0e306e7614e8328150f04
SawyerWomack <sawyerwomack@gmail.com>
2022-10-26 08:52:11 -0700

    Finished writin and ttesting the scriptt. 2 function  were solved using a config file

--- WindowsScript.bat
+++ WindowsScript.bat
@@ -18,7 +18,7 @@ net session
 	echo "1) Scan for Media Files		2) change all passwords				   "
 	echo "3) disable guest account		4) Password Policy					   "
 	echo "5) Lockout Policy			    6) Enable Firewall  				   "
-	echo "7) User Rights        		8) Disable Services				   	   "
+	echo "       		8) Disable Services				   	   "
 	echo "9) Remote Desktop Config		10) Auto Update				   		   "
 	echo "11) Security Options  		12) Audit Policy				       "
 	echo "69) Exit						70) Reboot							   "
@@ -26,17 +26,27 @@ net session
 	set /p answer=Please choose an option: 
 	if "%answer%"=="69" exit
 	if "%answer%"=="70" shutdown /r
-	if "%answer%"=="1" goto :media
+	::works
+	if "%answer%"=="1" goto :media  
+	::dosnt work
 	if "%answer%"=="2" goto :changePasswords
+	::works
 	if "%answer%"=="3" goto :disableGuest
+	::works
 	if "%answer%"=="4" goto :passwordPolicy
+	::works
 	if "%answer%"=="5" goto :lockoutPolicy
+	::works
 	if "%answer%"=="6" goto :enableFirewall
-	if "%answer%"=="7" goto :userRights
+	::works
 	if "%answer%"=="8" goto :disableServices
+	::works
 	if "%answer%"=="9" goto :remoteDesktop
+	:: works
 	if "%answer%"=="10" goto :autoUpdate
+	:: works
 	if "%answer%"=="11" goto :securityOptions
+	:: works
 	if "%answer%"=="12" goto :auditPolicy
 
 
@@ -79,6 +89,8 @@ net session
 	net accounts /maxpwage:60
 	net accounts /minpwage:10
 	net accounts /uniquepw:3
+	::opens the secpol window
+	secpol.msc
 	echo Enable password must meet complexity requirments
 	pause
 	echo  disable store password using reversible encyrption
@@ -91,14 +103,15 @@ net session
 	net accounts /lockoutduration:30
 	net accounts /lockoutthreshold:3
 	net accounts /lockoutwindow:15
+	::opens the secpol window
+	secpol.msc
 	goto :display
 
 :enableFirewall
 	NetSh Advfirewall set allprofiles state on
 	Netsh Advfirewall show allprofiles
 
-:userRights
-	goto :display
+
 
 :disableServices
 	echo Disabling Services
@@ -139,26 +152,143 @@ net session
 	goto :display
 
 :remoteDesktop
-	echo find bad files
+	set /p answer=Do you want remote desktop enabled?[y/n]
+	if /I "%answer%"=="y" (
+		reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+		echo RemoteDesktop has been enabled, reboot for this to take full effect.
+	)
+	if /I "%answer%"=="n" (
+		reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+		echo RemoteDesktop has been disabled, reboot for this to take full effect.
+	)
 	goto :display
 
 :autoUpdate
-	echo find bad files
+	explorer ms-settings:windowsupdate-action
+
 	goto :display
 
 :securityOptions
-	echo find bad files
+	rem Restrict CD ROM drive
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateCDRoms /t REG_DWORD /d 1 /f
+
+	rem Automatic Admin logon
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+	
+	rem Logon message text
+	set /p body=Please enter logon text: 
+		reg ADD "HKLM\SYSTEM\microsoft\Windwos\CurrentVersion\Policies\System\legalnoticetext" /v LegalNoticeText /t REG_SZ /d "%body%"
+	
+	rem Logon message title bar
+	set /p subject=Please enter the title of the message: 
+		reg ADD "HKLM\SYSTEM\microsoft\Windwos\CurrentVersion\Policies\System\legalnoticecaption" /v LegalNoticeCaption /t REG_SZ /d "%subject%"
+	
+	rem Wipe page file from shutdown
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+	
+	rem Disallow remote access to floppie disks
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
+	
+	rem Prevent print driver installs 
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
+	
+	rem Limit local account use of blank passwords to console
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+	
+	rem Auditing access of Global System Objects
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v auditbaseobjects /t REG_DWORD /d 1 /f
+	
+	rem Auditing Backup and Restore
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v fullprivilegeauditing /t REG_DWORD /d 1 /f
+	
+	rem Do not display last user on logon
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+	
+	rem UAC setting (Prompt on Secure Desktop)
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
+	
+	rem Enable Installer Detection
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
+	
+	rem Undock without logon
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v undockwithoutlogon /t REG_DWORD /d 0 /f
+	
+	rem Maximum Machine Password Age
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v MaximumPasswordAge /t REG_DWORD /d 15 /f
+	
+	rem Disable machine account password changes
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v DisablePasswordChange /t REG_DWORD /d 1 /f
+	
+	rem Require Strong Session Key
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireStrongKey /t REG_DWORD /d 1 /f
+	
+	rem Require Sign/Seal
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireSignOrSeal /t REG_DWORD /d 1 /f
+	
+	rem Sign Channel
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SignSecureChannel /t REG_DWORD /d 1 /f
+	
+	rem Seal Channel
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SealSecureChannel /t REG_DWORD /d 1 /f
+	
+	rem Don't disable CTRL+ALT+DEL even though it serves no purpose
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 0 /f 
+	
+	rem Restrict Anonymous Enumeration #1
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f 
+	
+	rem Restrict Anonymous Enumeration #2
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f 
+	
+	rem Idle Time Limit - 45 mins
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v autodisconnect /t REG_DWORD /d 45 /f 
+	
+	rem Require Security Signature - Disabled pursuant to checklist
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v enablesecuritysignature /t REG_DWORD /d 0 /f 
+	
+	rem Enable Security Signature - Disabled pursuant to checklist
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v requiresecuritysignature /t REG_DWORD /d 0 /f 
+	
+	rem Disable Domain Credential Storage
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f 
+	
+	rem Don't Give Anons Everyone Permissions
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f 
+	
+	rem SMB Passwords unencrypted to third party
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
+	
+	rem Null Session Pipes Cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
+	
+	rem remotely accessible registry paths cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
+	
+	rem remotely accessible registry paths and sub-paths cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
+	
+	rem Restict anonymous access to named pipes and shares
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
+	
+	rem Allow to use Machine ID for NTLM
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v UseMachineId /t REG_DWORD /d 0 /f
+
+	rem Enables DEP
+	bcdedit.exe /set {current} nx AlwaysOn
+	pause
+
 	goto :display
 
 :auditPolicy
 	auditpol /set /category:"Account Logon" /Success:enable /failure:enable >nul
 	auditpol /set /category:"Logon/Logoff" /Success:enable /failure:enable >nul
 	auditpol /set /category:"Account Management" /Success:enable /failure:enable >nul
-	Auditpol /set /category:"DS Access" /failure:enable >nul
-	Auditpol /set /category:"Object Access" /failure:enable >nul
+	Auditpol /set /category:"DS Access" /Success:enable /failure:enable >nul
+	Auditpol /set /category:"Object Access" /Success:enable /failure:enable >nul
 	Auditpol /set /category:"policy change" /Success:enable /failure:enable >nul
 	Auditpol /set /category:"Privilege use" /Success:enable /failure:enable >nul
-	Auditpol /set /category:"System" /failure:enable >nul
+	Auditpol /set /category:"System" /Success:enable /failure:enable >nul
+	Auditpol /set /category:"Detailed Tracking" /Success:enable /failure:enable >nul
 	goto :display
 
 
