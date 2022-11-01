0264d4ac92994db3ff4d354de4b9599df1d79d75
sawyerwomack <sawyerwomack@gmail.com>
2022-10-31 21:40:55 -0400

    finished alot of work but unfortunatley i accidently changed the password of the root accountof my vm to a random set of characters and now idk the password

--- UbuntuScript.py
+++ UbuntuScript.py
@@ -1,8 +1,163 @@
 import os
 
+running = True
+
+password = "CyberPatriots23$$"
+
+functions = [
+    "Update",
+    "ManageUsers",
+    "pFiles",
+    "autoUpdate",
+    "firewallConfig",
+    "chngPasswd"
+]
+
+
+
+badFileTypes = [
+    "mov",
+    "mp4",
+    "mp3",
+    "wav",
+    "jpg",
+    "jpeg",
+    "tar.gz",
+    "php",
+    "backdoor*.*"
+
+]
+
+
+sysUsers = []
+
+users = str(os.popen("cut -d: -f1,3 /etc/passwd | egrep \':[0-9]{4}$\' | cut -d: -f1").read())
+    #removesdafault users
+sysUsers = users.split("\n")
+
+#removes a empty string in the array
+sysUsers.sort()
+sysUsers.pop(0)
+
+print(sysUsers)
 
 
 def test():
+    os.popen("cat users")
+
+def display():
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("			  _________                 ________.____________ 		   ")
+    print("			 /   _____/ ____           /   __   \   ____/_   |		   ")
+    print("			 \_____  \_/ ___\   ______ \____    /____  \ |   |		   ")
+    print("			 /        \  \___  /_____/    /    //       \|   |		   ")
+    print("			/_______  /\___  >           /____//______  /|___|		   ")
+    print("			        \/     \/                         \/      		   ")
+    print("                                                            		   ")
+    print("~~~~~~~~~~~~~Written by: Sawyer Womack AFJROTC SC-951~~~~~~~~~~~~~~~")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("1) Update		            2) Manage Users				           ")
+    print("3) find prohibited files 	4) Set auto updates					   ")
+    print("5)Congure the firewall		6) Change all passwords 		       ")
+    print("69) Exit						70) Reboot							   ")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    num = int(input("enter command: "))
+    num -= 1
+    if(num != 68):
+        eval(functions[num] + "()")
+        return True
+    else:
+        return False
     
 
-test()
+    
+
+def Update():
+    os.system("sudo apt-get update -y")
+    os.system("wait")
+    os.system("sudo apt-get update -y")
+    os.system("wait")
+    os.system("sudo apt-get upgrade -y")
+    os.system("wait")
+    os.system("sudo apt-get dist-upgrade -y")
+    os.system("wait")
+    os.system("killall firefox")
+    os.system("wait")
+    os.system("sudo apt-get --purge --reinstall install firefox -y")
+    os.system("wait")
+    os.system("sudo apt-get install clamtk -y	")
+    os.system("wait")
+    
+
+def ManageUsers():
+    
+
+    inputUsers = input("please copy and paste the user part of the readme:")
+    
+
+    readMeUsers = inputUsers.split(" ")
+    
+    
+    #sorts the arrays so that the arrays contain the names in the same order
+    
+    readMeUsers.sort()
+    print(sysUsers)
+    
+    #readme to system
+    
+    for i in range(0,len(readMeUsers)):
+        found = False
+        for x in range(0, len(sysUsers)):
+            if(readMeUsers[i] == sysUsers[x]):
+                found = True
+                break
+        if(found == False):
+            print(readMeUsers[i] + " is not in the system")
+        
+    
+
+            
+    #system to readme
+
+    for i in range(0,len(sysUsers)):
+        found = False
+        for x in range(0, len(readMeUsers)):
+            if(readMeUsers[x] == sysUsers[i]):
+                found = True
+                break
+        if(found == False):
+            print(sysUsers[i] + " is not in the readme")
+    input()
+
+
+def autoUpdate():
+    os.system("sed -i -e 's/APT::Periodic::Update-Package-Lists.*\+/APT::Periodic::Update-Package-Lists \"1\";/' /etc/apt/apt.conf.d/10periodic")
+    os.system("sed -i -e 's/APT::Periodic::Download-Upgradeable-Packages.*\+/APT::Periodic::Download-Upgradeable-Packages \"0\";/' /etc/apt/apt.conf.d/10periodic")
+    os.system("sed -i 's/x-scheme-handler\/http=.*/x-scheme-handler\/http=firefox.desktop/g' /home/$UserName/.local/share/applications/mimeapps.list")
+
+def pFiles():
+    for i in range(0, len(badFileTypes)):
+        os.system("sudo find / -name \"*" + badFileTypes[i] + "\" -type f >> pFiles.log")
+
+def firewallConfig():
+    os.system("sudo echo sucsess")
+
+
+def chngPasswd():
+    for i in range(0,len(sysUsers)):
+        os.system("sudo echo -e " + password + "\n" + password + " | passwd " + sysUsers[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+while(running):
+    running = display()
